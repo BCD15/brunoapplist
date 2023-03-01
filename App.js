ec6bc4df615f8a9089f6eb233fd8a066994d43ab
BCD15 <bruno69correadias@gmail.com>
2023-03-01 11:51:30 -0300

    deucertodnovo

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './src/components/Header';
 export default function App() {
+  const tasks = ['Tarefa 1', 'Tarefa 2', 'Tarefa 3', 'Tarefa 4', 'Tarefa 5'];
   return (
     <View style={styles.container}>
-      <Text>Deu certo!</Text>
-      <StatusBar style="auto" />
+      <Header />
+        <FlatList data={tasks} renderItem={({ item }) => <Text>{item}</Text>} />
     </View>
   );
 }
